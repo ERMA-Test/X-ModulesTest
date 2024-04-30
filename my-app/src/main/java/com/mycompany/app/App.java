63d3c5f2f842f206fadc38a3b4f50b15a3dca44e
emanz <erik.manz@doubleslash.de>
2024-04-30 14:28:31 +0200

    changed commit message

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello World! why would anybody use git subtrees?" );
     }
 }
