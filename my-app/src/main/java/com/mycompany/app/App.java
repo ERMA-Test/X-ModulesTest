216e22b8874bea21a835a74f5c9fe32b63271f2d
emanz <erik.manz@doubleslash.de>
2024-04-30 14:10:36 +0200

    changes commit message again

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! Submodules are great" );
+        System.out.println( "Hello World! Submodules are great und fun" );
     }
 }
