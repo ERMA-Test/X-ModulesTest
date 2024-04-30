5cfcb321893db5f7cc31d18904587b769e03fca3
emanz <erik.manz@doubleslash.de>
2024-04-30 14:49:57 +0200

    i am the danger

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -9,5 +9,8 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World! why would anybody use git subtrees?" );
+        for (int i = 0; i < 10; i++) {
+            System.out.println("I am the danger");
+        }
     }
 }
