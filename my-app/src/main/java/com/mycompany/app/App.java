a2a803b9891644a60835ed3d607632a79cffdb8d
emanz <erik.manz@doubleslash.de>
2024-04-30 14:17:28 +0200

    5x i like submosules

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -9,5 +9,8 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World! Submodules are great und fun but a little bit strange" );
+        for (int i = 0; i < 5; i++) {
+            System.out.println("I like submodules");
+        }
     }
 }
